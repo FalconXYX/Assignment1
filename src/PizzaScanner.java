33d2f5d0732396390b39981b3d183fdbe0ccfcba
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-15 12:35:18 -0400

    stuff

--- src/PizzaScanner.java
+++ src/PizzaScanner.java
@@ -1,12 +1,27 @@
 
+import java.util.Scanner;
+
+
 public class PizzaScanner {
 
     
     public static void main(String[] args) {
-        final double LCOST = 7;
-        final double UCOST = 7;
-        double tcost, bcost;
+        final double LCOST = 1.5;
+        final double UCOST = 0.75;
+        double tcost, bcost,totalcost;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the amount of toppings you want: ");
+        tcost = 0.75*(s.nextInt());
+        System.out.print("Enter the size of pizza you want: ");
+        bcost = 0.50*(s.nextInt());
+        totalcost = tcost+bcost+UCOST+LCOST;
         
+        
+        System.out.println(String.format("The labour cost of the Pizza was: %.2f", LCOST));
+        System.out.println(String.format("The Utilities cost of the Pizza was: %.2f", UCOST));
+        System.out.println(String.format("The base cost of the Pizza was: %.2f", bcost));
+        System.out.println(String.format("The toppings cost of the Pizza was: %.2f", tcost));
+        System.out.println(String.format("The total cost of the Pizza is: %.2f", totalcost));
         
         
     
