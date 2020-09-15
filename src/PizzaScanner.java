20beaa9333a62d60cceffc2e38ad386581701a0e
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-15 11:59:29 -0400

    stuff

--- src/PizzaScanner.java
+++ src/PizzaScanner.java
@@ -5,6 +5,9 @@ public class PizzaScanner {
     public static void main(String[] args) {
         final double LCOST = 7;
         final double UCOST = 7;
+        double tcost, bcost
+        
+        
     
     
 }
