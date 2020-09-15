2c9da7539875ccbe83e3a3fe47f65d13cdfbeae9
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-15 11:54:35 -0400

    stuff

--- src/PizzaScanner.java
+++ src/PizzaScanner.java
@@ -3,6 +3,13 @@ public class PizzaScanner {
 
     
     public static void main(String[] args) {
-    }
+        final double LCOST = 7;
+        final double UCOST = 7;
+    
     
 }
+}
+//All pizzas have a fixed cost of $1.50 for labor to make the pizza (use a constant)
+// All pizzas use $0.75 worth of gas heat for the pizza oven (use a constant)
+// Toppings cost $0.75 each
+// Basic ingredients (sauce, dough, cheese) cost $0.50 per inch of pizza diameter 
