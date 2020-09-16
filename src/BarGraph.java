5e477c485ce930d2312a934c8d98b644d1be2e38
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-16 09:27:41 -0400

    stuff

--- src/BarGraph.java
+++ src/BarGraph.java
@@ -1,20 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author part8880
- */
+import TurtleGraphics.*;
+import java.awt.Color;
+import java.util.Scanner;
+
 public class BarGraph {
 
-    /**
-     * @param args the command line arguments
-     */
+    static SketchPadWindow window = new SketchPadWindow(800, 600);
+    static Pen p = new StandardPen(window);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int v1, v2, v3, v4;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter Value 1: ");
+        v1 = s.nextInt();
+        System.out.print("Enter Value 2: ");
+        v2 = s.nextInt();
+        System.out.print("Enter Value 3: ");
+        v3 = s.nextInt();
+        System.out.print("Enter Value 4: ");
+        v4 = s.nextInt();
+        p.setWidth(8);
+        p.setColor(Color.black);
+        p.up();
+        p.move(-300, 250);
+        p.down();
+        p.move(-300, -250);
+        p.move(550, -250);
+        makebar(Color red,v1, -200,-250);
+        makebar(Color red,v1, -100,-250);
+        makebar(Color red,v1, 0,-250);
+        makebar(Color red,v1, 100,-250);
+        
+        
     }
-    
+
+    public static void makebar(Color col, int l, int y, int x) {
+        p.setWidth(8);
+        p.up();
+        p.move(x, y);
+        p.down();
+        p.setColor(Color.col);
+        p.setDirection(90);
+        p.move(l);
+        p.up();
+        p.move(5);
+        p.down();
+        p.drawString(l+"");
+        
+
+    }
+
 }
