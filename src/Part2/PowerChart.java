2b52ed67d1e4a374bdd9c1a93d036c3a40464c7b
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-17 09:57:43 -0400

    stuff

--- src/Part2/PowerChart.java
+++ src/Part2/PowerChart.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class PowerChart {
     public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+        int input, answer;
+        while(true){
+        System.out.print("Enter Number Between 1-10");
+        input = s.nextInt();
+        if(input > 0 && input < 11){break;}
+       
+        }
+        
+        System.out.println("X \t 2^X");
+        System.out.println("__ \t ____");
+        for (int i = 0; i <= input; i++) {
+            answer  = (int) Math.pow(2, i);
+            System.out.println(String.format("%d \t 2^%d=%d", i,i,answer));
+                    
+            
+            
+        }
+        
        
       
     }
